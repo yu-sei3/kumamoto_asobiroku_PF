a1a67a7253d494d3afb9847eb5bc72fe6869a768
内田勇征 <96036914+yu-sei3@users.noreply.github.com>
2022-03-06 05:42:29 +0000

    [add]Model

---
+++ db/migrate/20220306051953_create_post_images.rb
@@ -0,0 +1,12 @@
+class CreatePostImages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :post_images do |t|
+      t.integer :end_user_id, null: false
+      t.integer :genre_id, null: false
+      t.string :body, null: false
+      t.string :location, null: false
+
+      t.timestamps
+    end
+  end
+end
