1c1514990cffcc3380c7e7dd828ed3313a409755
内田勇征 <96036914+yu-sei3@users.noreply.github.com>
2022-03-07 04:22:11 +0000

    [add]アソシエーション

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -3,4 +3,31 @@ class EndUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :post_images, dependent: :destroy
+
+  has_many :post_comments, dependent: :destroy
+
+  has_many :favorites, dependent: :destroy
+
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォローされる側を取得
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # フォローしてくる側を取得
+  has_many :following_end_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_end_user, through: :followed, source: :follower  # 自分をフォローしている人
+
+  # ユーザーをフォローする
+  def follow(end_user_id)
+    follower.create(followed_id: end_user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(end_user_id)
+    follower.find_by(followed_id: end_user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(end_user)
+    following_end_user.include?(end_user)
+  end
+
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,4 +1,12 @@
 class PostImage < ApplicationRecord
-  belongs_to :follower
-  belongs_to :followed
+
+  belongs_to :end_user
+  belongs_to :genre
+
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(end_user)
+    favorites.exists?(end_user_id: end_user.id)
+  end
+
 end
