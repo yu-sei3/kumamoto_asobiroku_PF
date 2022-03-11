e540c865b0bfc5467e62fafb3c3e8776f35904a4
内田勇征 <96036914+yu-sei3@users.noreply.github.com>
2022-03-11 07:23:49 +0000

    [add]Favorites

---
+++ app/controllers/public/favorites_controller.rb
@@ -0,0 +1,17 @@
+class Public::FavoritesController < ApplicationController
+
+  def create
+    post_image = PostImage.find(params[:post_image_id])
+    favorite = current_end_user.favorites.new(post_image_id: post_image.id)
+    favorite.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    post_image = PostImage.find(params[:post_image_id])
+    favorite = current_end_user.favorites.find_by(post_image_id: post_image.id)
+    favorite.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+end
