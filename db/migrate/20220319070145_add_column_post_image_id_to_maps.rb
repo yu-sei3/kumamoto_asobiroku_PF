867b352082f14b5aae69abc2c4832483bfb4b5c9
内田勇征 <96036914+yu-sei3@users.noreply.github.com>
2022-03-19 08:46:12 +0000

    [Remove]add_column_file

--- db/migrate/20220319070145_add_column_post_image_id_to_maps.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnPostImageIdToMaps < ActiveRecord::Migration[6.1]
-  def change
-    add_column :maps, :post_image_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_19_070145) do
+ActiveRecord::Schema.define(version: 2022_03_18_070438) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -92,7 +92,6 @@ ActiveRecord::Schema.define(version: 2022_03_19_070145) do
     t.float "longitube"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "post_image_id"
   end
 
   create_table "post_comments", force: :cascade do |t|
